1d8ac4fa160320e9acc4213dde4d9da9de724129
Heather Nuffer <zzzuzz@gmail.com>
2019-12-03 07:32:38 -0700

    Additional styling and state in HouseInfo.js

--- react-sorting-hat/src/components/HouseInfo.js
+++ react-sorting-hat/src/components/HouseInfo.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React , { useEffect, useState } from 'react';
 import houseData from '../houseData';
 
 const HouseInfo = ( {house}) => {
-    let correctHouse = {};
-    houseData.forEach(item => {
-        if (item.id === house) {
-            correctHouse = item;
-        }
-    });
+    const [correctHouse, setCorrectHouse] = useState('G');
+    const [correctHouseName, setCorrectHouseName] = useState('gryffindor');
+
+    useEffect(() => {
+        houseData.forEach(item => {
+            if (item.id === house) {
+                setCorrectHouse(item);
+                setCorrectHouseName(item.name);
+            }
+        });
+    }, [house]);
+    
     return (
         <div className='houseInfo' style={{
             borderTop: `10px solid ${correctHouse.color1}`,
@@ -16,14 +22,14 @@ const HouseInfo = ( {house}) => {
             borderBottom: `10px solid ${correctHouse.color1}`,
             }}>
             <h2>"{correctHouse.name}!!!!" shouts the hat.</h2>
-            <h3>Here's more about {correctHouse.name}:</h3>
+            <h3>Here's more about {correctHouseName}:</h3>
             <p>{correctHouse.peopleType}</p>
-            <p>Founder: {correctHouse.founder}</p>
-            <p>Headmaster: {correctHouse.headmaster}</p>
-            <p>Element: {correctHouse.element}</p>
-            <p>House Ghost: {correctHouse.ghost}</p>
-            <p>Animal: {correctHouse.animal}</p>
-            <p>The common room is located {correctHouse.commonRoomLocation}</p>
+            <p><span className='bold'>Founder:</span> {correctHouse.founder}</p>
+            <p><span className='bold'>Headmaster:</span> {correctHouse.headmaster}</p>
+            <p><span className='bold'>Element:</span> {correctHouse.element}</p>
+            <p><span className='bold'>House Ghost:</span> {correctHouse.ghost}</p>
+            <p><span className='bold'>Animal:</span> {correctHouse.animal}</p>
+            <p>The <span className='bold'>common room</span> is located {correctHouse.commonRoomLocation}</p>
             <p>It is {correctHouse.commonRoomDescription}</p>
             <p>The entrance is {correctHouse.commonRoomEntrance}</p>  
         </div>

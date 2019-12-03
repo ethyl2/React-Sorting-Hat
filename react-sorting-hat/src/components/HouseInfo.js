677c3c9336e549289f3739ab23fc535abb4169a9
Heather Nuffer <zzzuzz@gmail.com>
2019-12-03 09:03:33 -0700

    Added axios call to API for characters' info

--- react-sorting-hat/src/components/HouseInfo.js
+++ react-sorting-hat/src/components/HouseInfo.js
@@ -1,9 +1,11 @@
 import React , { useEffect, useState } from 'react';
 import houseData from '../houseData';
+import axios from 'axios';
 
 const HouseInfo = ( {house}) => {
     const [correctHouse, setCorrectHouse] = useState('G');
     const [correctHouseName, setCorrectHouseName] = useState('gryffindor');
+    const [characters, setCharacters] = useState([]);
 
     useEffect(() => {
         houseData.forEach(item => {
@@ -13,6 +15,15 @@ const HouseInfo = ( {house}) => {
             }
         });
     }, [house]);
+
+    useEffect(()=> {
+        axios.get(`https://hp-api.herokuapp.com/api/characters/house/gryffindor`)
+            .then(response => {
+                console.log(response);
+                setCharacters(response.data);
+            })
+            .catch(err => console.log(err));
+    }, [correctHouse]);
     
     return (
         <div className='houseInfo' style={{
@@ -21,8 +32,8 @@ const HouseInfo = ( {house}) => {
             borderRight: `10px solid ${correctHouse.color2}`, 
             borderBottom: `10px solid ${correctHouse.color1}`,
             }}>
-            <h2>"{correctHouse.name}!!!!" shouts the hat.</h2>
-            <h3>Here's more about {correctHouseName}:</h3>
+            <h1>"{correctHouse.name}!!!!" shouts the hat.</h1>
+            <h2>More About {correctHouseName}</h2>
             <p>{correctHouse.peopleType}</p>
             <p><span className='bold'>Founder:</span> {correctHouse.founder}</p>
             <p><span className='bold'>Headmaster:</span> {correctHouse.headmaster}</p>
@@ -31,7 +42,19 @@ const HouseInfo = ( {house}) => {
             <p><span className='bold'>Animal:</span> {correctHouse.animal}</p>
             <p>The <span className='bold'>common room</span> is located {correctHouse.commonRoomLocation}</p>
             <p>It is {correctHouse.commonRoomDescription}</p>
-            <p>The entrance is {correctHouse.commonRoomEntrance}</p>  
+            <p>The entrance is {correctHouse.commonRoomEntrance}</p>
+
+            <div>
+                <h2>Fellow {correctHouseName}s</h2>
+                {characters.map(character => {
+                    return (
+                        <div key={character.name}>
+                            <h3>{character.name}</h3>
+                            <img src={character.image} alt='bio pic' />
+                        </div>
+                    )
+                })}
+            </div>  
         </div>
     )
 }

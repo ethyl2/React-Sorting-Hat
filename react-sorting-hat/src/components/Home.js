a271f60353ec16e79f48a7eda4a5ee3bb34818bc
Heather Nuffer <zzzuzz@gmail.com>
2019-12-04 14:47:49 -0700

    Added image folder

--- react-sorting-hat/src/components/Home.js
+++ react-sorting-hat/src/components/Home.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import candle from '../images/candle.svg';
 
 const Home = () => {
     return(
         <div>
+            <div className='imgContainer icon'>
+                <img src={candle} alt='candle' />
+            </div>
+            <div className='imgContainer icon'>
+                <img src={candle} alt='candle' />
+            </div>
+            <div className='imgContainer icon'>
+                <img src={candle} alt='candle' />
+            </div>
             <p className="App-intro">
             To get started, click the button, if you are ready!
             </p>
             <Link to='/quiz'>Put on the Hat</Link>
+            <br />
+            <div className='imgContainer icon'>
+                <img src={candle} alt='candle' />
+            </div>
+            <div className='imgContainer icon'>
+                <img src={candle} alt='candle' />
+            </div>
+            <div className='imgContainer icon'>
+                <img src={candle} alt='candle' />
+            </div>
         </div>
     )
 }
